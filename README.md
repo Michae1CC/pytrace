f63962a562462c2fe351b2dd1f752fa0316ff6bd
Michael CC <333mmcc@gmail.com>
2025-07-06 16:42:05 +1000

    Happy path working

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # pytrace
 
-A networking tool to perform route tracing
+A networking tool to perform route tracing.
+
+```text
+usage: pytrace [-h] [-s src_addr] [-f first_ttl] [-m max_ttl] [-p port] [-q nqueries]
+               [-w waittime] [-z pausemsecs]
+               host [packet_length]
+
+positional arguments:
+  host
+  packet_length
+
+options:
+  -h, --help     show this help message and exit
+  -s src_addr    Use the following IP address (which must be given as an IP number, not a
+                 hostname) as the source address in outgoing probe packets. On hosts with
+                 more than one IP address, this option can be used to force the source
+                 address to be something other than the IP address of the interface the
+                 probe packet is sent on. If the IP address is not one of this machine's
+                 interface addresses, an error is returned and nothing is sent.
+  -f first_ttl   Set the initial time-to-live used in the first outgoing probe packet. The
+                 default is 1, .i.e., start with the first hop.
+  -m max_ttl     Set the max time-to-live (max number of hops) used in outgoing probe
+                 packets. The default is 64 hops.
+  -p port        Sets the base port used in probes (default is 33434).
+  -q nqueries    Set the number of queries per 'ttl' to nqueries (default is 3 probes)
+  -w waittime    Set the time (in seconds) to wait for a response to a probe (default 5
+                 sec.).
+  -z pausemsecs  Set the time (in milliseconds) to pause between probes (default 0). Some
+                 systems such as Solaris and routers such as Ciscos rate limit ICMP
+                 messages. A good value to use with this is 500 (e.g. 1/2 second).
+```
 
 ## References
 
--- pytrace/main.py
+++ pytrace/main.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 import argparse
-import sys
 import socket
 import struct
+import sys
+import time
 
 from collections.abc import Sequence
 
@@ -94,6 +95,10 @@ def _send_pings(
     max_ttl: int,
     port: int,
     nqueries: int,
+    wait_time: int,
+    pause_msec: int,
+    host: str,
+    packet_length: int | None,
     src_addr: str | None,
 ) -> None:
     socket_protocol: int = (
@@ -101,41 +106,81 @@ def _send_pings(
         if address_family == socket.AF_INET
         else socket.IPPROTO_ICMPV6
     )
-    print(f"socket.AF_INET {socket.AF_INET}")
-    print(f"socket.AF_INET6 {socket.AF_INET6}")
-    print(f"socket.IPPROTO_ICMP {socket.IPPROTO_ICMP}")
-    print(f"socket.IPPROTO_ICMPV6 {socket.IPPROTO_ICMPV6}")
+    # print(f"socket.AF_INET {socket.AF_INET}")
+    # print(f"socket.AF_INET6 {socket.AF_INET6}")
+    # print(f"socket.IPPROTO_ICMP {socket.IPPROTO_ICMP}")
+    # print(f"socket.IPPROTO_ICMPV6 {socket.IPPROTO_ICMPV6}")
 
-    print(address_family)
-    print(socket_protocol)
+    # print(address_family)
+    # print(socket_protocol)
 
-    # Create the socket
-    with socket.socket(address_family, socket.SOCK_DGRAM, socket_protocol) as sock:
-        icmp_echo_message = ICMPEchoMessage(identifier=1, sequence_number=1)
-        sock.sendto(bytes(icmp_echo_message), ("1.1.1.1", 0))
-        try:
+    print(
+        f"pytrace to {host} ({host}), {max_ttl} hops max, {packet_length} bytes packets"
+    )
+
+    for ttl in range(first_ttl, max_ttl + 1):
+
+        got_to_dest: bool = False
+
+        print(f"{ttl}", end=" ")
+
+        # Create the socket
+        with socket.socket(address_family, socket.SOCK_DGRAM, socket_protocol) as sock:
+            sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
+            icmp_echo_message = ICMPEchoMessage(identifier=1, sequence_number=0)
             sock.settimeout(1)  # Timeout for receiving reply
-            recv_packet, addr = sock.recvfrom(1024)
-            icmp_header = recv_packet[20:28]  # IP header is usually 20 bytes
-            icmp_type, icmp_code, _, _, _ = struct.unpack("!BBHHH", icmp_header)
 
-            if icmp_type == 0 and icmp_code == 0:  # Echo Reply
-                print(f"Received ICMP Echo Reply from {addr[0]}")
-            else:
-                print(f"Received unexpected ICMP type/code: {icmp_type}/{icmp_code}")
-        except socket.timeout:
-            print(f"Timeout waiting for reply")
-        except Exception as e:
-            print(f"Error receiving reply: {e}")
+            for prod_num in range(nqueries):
+
+                if prod_num != 0:
+                    time.sleep(pause_msec)
+
+                packet_sent_time: float = time.perf_counter()
+                sock.sendto(bytes(icmp_echo_message), (host, 0))
+
+                try:
+                    recv_packet, addr = sock.recvfrom(1024)
+                    packet_received_time: float = time.perf_counter()
+                except socket.timeout:
+                    print(" *", end="")
+                    continue
+
+                # https://datatracker.ietf.org/doc/html/rfc791#section-3.1
+                ip_header: bytes = recv_packet[0:20]
+                ip_header_struct = struct.unpack("!BBHHHBBH4s4s", ip_header)
+                icmp_header = recv_packet[20:28]  # ICMP header is usually 20 bytes
+                icmp_type, icmp_code, _, _, _ = struct.unpack("!BBHHH", icmp_header)
+
+                source_address = socket.inet_ntoa(ip_header_struct[8])
+
+                if prod_num == 0:
+                    print(f"{source_address} ({source_address})", end=" ")
+
+                print(
+                    "{:.3f} ms".format(
+                        (packet_received_time - packet_sent_time) * 1000
+                    ),
+                    end=" ",
+                )
+
+                if icmp_type == 0:
+                    got_to_dest = True
+
+        print()
+
+        if got_to_dest:
+            break
 
 
 def main(argv: Sequence[str] | None = None) -> None:
+
+    # 128.32.131.22
+
     argv = argv if argv is not None else sys.argv[1:]
     parser = argparse.ArgumentParser(prog="pytrace")
 
     parser.add_argument(
         "-s",
-        "--src_addr",
         type=str,
         help=(
             "Use the following IP address (which must be given as an IP number, "
@@ -147,47 +192,53 @@ def main(argv: Sequence[str] | None = None) -> None:
             "error is returned and nothing is sent."
         ),
         default=None,
+        metavar="src_addr",
+        dest="src_addr",
     )
 
     parser.add_argument(
         "-f",
-        "--first_ttl",
         type=int,
         help=(
             "Set the initial time-to-live used in the first outgoing probe packet. "
             "The default is 1, .i.e., start with the first hop."
         ),
         default="1",
+        metavar="first_ttl",
+        dest="first_ttl",
     )
 
     parser.add_argument(
         "-m",
-        "--max_ttl",
         type=int,
         help=(
             "Set the max time-to-live (max number of hops) used in outgoing probe "
             f"packets. The default is {DEFAULT_MAX_TTL} hops."
         ),
         default=f"{DEFAULT_MAX_TTL}",
+        metavar="max_ttl",
+        dest="max_ttl",
     )
 
     parser.add_argument(
         "-p",
-        "--port",
         type=int,
         help=(f"Sets the base port used in probes (default is {DEFAULT_PORT})."),
         default=f"{DEFAULT_PORT}",
+        metavar="port",
+        dest="port",
     )
 
     parser.add_argument(
         "-q",
-        "--nqueries",
         type=int,
         help=(
             "Set the number of queries per 'ttl' to nqueries "
             f"(default is {DEFAULT_NUMBER_OF_QUERIES} probes)"
         ),
         default=f"{DEFAULT_NUMBER_OF_QUERIES}",
+        metavar="nqueries",
+        dest="nqueries",
     )
 
     parser.add_argument(
@@ -198,6 +249,8 @@ def main(argv: Sequence[str] | None = None) -> None:
             f"(default {DEFAULT_RESPONSE_WAIT_TIME_SEC} sec.)."
         ),
         default=f"{DEFAULT_RESPONSE_WAIT_TIME_SEC}",
+        metavar="waittime",
+        dest="wait_time",
     )
 
     parser.add_argument(
@@ -209,13 +262,11 @@ def main(argv: Sequence[str] | None = None) -> None:
             "A good value to use with this is 500 (e.g. 1/2 second)."
         ),
         default=f"{DEFAULT_PAUSE_TIME_MSEC}",
+        metavar="pausemsecs",
+        dest="pause_msecs",
     )
 
-    parser.add_argument(
-        "host",
-        nargs=1,
-        type=str,
-    )
+    parser.add_argument("host", type=str)
 
     parser.add_argument(
         "packet_length",
@@ -243,5 +294,9 @@ def main(argv: Sequence[str] | None = None) -> None:
         max_ttl=args.max_ttl,
         port=args.port,
         nqueries=args.nqueries,
+        wait_time=args.wait_time,
+        pause_msec=args.pause_msecs,
+        host=args.host,
+        packet_length=args.packet_length,
         src_addr=src_addr,
     )
